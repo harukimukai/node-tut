f60ce1d50a14d3c01a18c41d95567077e2185736
Haruki Mukai <mukaiyousei@mukaiyouseisMBP.lan>
2025-03-14 17:01:46 -0500

    Done

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../../controllers/usersController');
 const ROLES_LIST = require('../../config/roles_list');
-const verifyRoles = require('../../middleware/verifyRoles');
+const verifyRoles = require('../../middleWare/verifyRoles');
 
 router.route('/')
     .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
